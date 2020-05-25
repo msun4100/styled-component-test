6dac17f490e72db26c5c5b616197264a4d68535c
msun4100 <msun4100@gmail.com>
2020-05-25 17:30:48 +0900

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle, css, keyframes } from "styled-components";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -8,35 +9,38 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
-`;
-
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: aqua;
 `;
 
-const Input = styled.input.attrs((props) => ({
-  type: "text",
-  required: true,
-}))`
-  border: none;
-  ${awesomeCard}
+const Card = styled.div`
+  background-color: red;
 `;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${(props) => props.theme.successColor};
+`;
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 
 class App extends Component {
   render() {
     return (
       <>
-        <GlobalStyle />
-        <Container>
-          <Input placeholder="hello" />
-        </Container>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Container>
+            <Form />
+          </Container>
+        </ThemeProvider>
       </>
     );
   }
