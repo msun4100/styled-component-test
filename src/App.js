bc3ff77bcea38486be0d80dcd829464d4eb5bc48
msun4100 <msun4100@gmail.com>
2020-05-25 17:13:29 +0900

    Animations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, css, keyframes } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -27,10 +27,26 @@ const Button = styled.button`
     outline: none;
   }
   background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
+  ${(props) => {
+    if (props.danger) {
+      return css`
+        animation: ${rotation} ${props.rotationTime}s linear infinite;
+      `;
+    }
+  }};
 `;
 
 const Anchor = styled(Button)`
   text-decoration: none;
+`;
+
+const rotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+  }
 `;
 
 class App extends Component {
@@ -39,8 +55,10 @@ class App extends Component {
       <>
         <GlobalStyle />
         <Container>
-          <Button success>Hello</Button>
-          <Button danger>Hello</Button>
+          <Button>Hello</Button>
+          <Button danger rotationTime={5}>
+            Hello
+          </Button>
           <Anchor as="a" href="https://google.com" target="_blank">
             Go to Google
           </Anchor>
